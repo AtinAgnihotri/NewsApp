f604ec9eef837432bf6440596b176989e267c799
AtinAgnihotri <atinagnihotri@gmail.com>
2021-09-05 12:58:57 +0530

    Adds ArticleTableViewCell

---
+++ NewsApp/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleTableViewCell.swift
+//  NewsApp
+//
+//  Created by Atin Agnihotri on 05/09/21.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
+
+
